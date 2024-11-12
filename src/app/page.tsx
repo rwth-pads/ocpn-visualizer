7ab2d5507a9f2ceec862f6b38e969b607f06315c
Tobias Brachmann <tobias.brachmann@rwth-aachen.de>
2024-11-12 08:50:49 +0100

    Added comments to functions and effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,58 +26,92 @@ const Home = () => {
     const [importError, setImportError] = useState<string | null>(null);
     const [selectedOCPN, setSelectedOCPN] = useState<number | null>(null);
 
+    /**
+     * Effect to update the dark mode state when the user changes their system preference.
+     */
     useEffect(() => {
         setDarkMode(prefersDarkMode);
     }, [prefersDarkMode]);
 
+    /**
+     * Effect to close the menu when the screen size is small.
+     */
     useEffect(() => {
         if (isSmallScreen) {
             setMenuOpen(false);
         }
     }, [isSmallScreen]);
 
+    /**
+     * Handler to toggle the dark mode state.
+     */
     const handleToggleDarkMode = () => {
         setDarkMode(!darkMode);
     };
 
+    /**
+     * Handler to toggle the tabs menu.
+     */
     const handleMenuClick = () => {
         setMenuOpen(!menuOpen);
     };
 
+    /**
+     * Handler to change the tab value.
+     * @param event The event that triggered the change.
+     * @param newValue The new tab value.
+     */
     const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
         setTabValue(newValue);
     };
 
+    /**
+     * Handler to open the import dialog where the user can import OCPNs.
+     */
     const handleImportClick = () => {
         setImportDialogOpen(true);
     };
 
+    /**
+     * Handler to close the import dialog.
+     */
     const handleImportClose = () => {
         setImportDialogOpen(false);
         setImportError(null);
     };
 
+    /**
+     * Handler to import a file that contains a valid OCPN.
+     * 
+     * @param file The file to import (must be a JSON or PNML file).
+     */
     const handleFileImport = async (file: File) => {
         const reader = new FileReader();
         reader.onload = async (e: ProgressEvent<FileReader>) => {
             const content = e.target?.result;
             try {
+                // Initialize the OCPN as null.
                 let ocpn: ObjectCentricPetriNet | null = null;
                 if (file.name.endsWith('.json')) {
+                    // File is a JSON file.
                     ocpn = ObjectCentricPetriNet.fromJSON(JSON.parse(content as string));
                 } else if (file.name.endsWith('.pnml')) {
+                    // File is a PNML file.
                     ocpn = await ObjectCentricPetriNet.fromPNML(content as string);
                 }
-
+                // Check if the OCPN is valid and not already imported.
                 if (ocpn) {
                     if (importedObjects.some(existingOCPN => existingOCPN.equals(ocpn))) {
+                        // OCPN is already imported.
                         setImportError('OCPN Already imported');
                     } else {
+                        // OCPN is valid and not already imported.
                         setImportedObjects((prev) => [...prev, ocpn]);
                         setSelectedOCPN(prev => prev === null ? 0 : prev);
                         handleImportClose();
                     }
                 } else {
+                    // Unsupported file type.
                     setImportError(`Unsupported file type: ${file.name}`);
                 }
             } catch (error) {
@@ -87,6 +121,11 @@ const Home = () => {
         reader.readAsText(file);
     };
 
+    /**
+     * Handler to import a file that contains a valid OCPN.
+     * 
+     * @param event The event that triggered the change.
+     */
     const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
@@ -94,6 +133,11 @@ const Home = () => {
         }
     };
 
+    /**
+     * Handler to handle the drop event when a file is dropped on the import dialog.
+     * 
+     * @param event The event that triggered the drop.
+     */
     const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         const file = event.dataTransfer.files?.[0];
@@ -102,6 +146,12 @@ const Home = () => {
         }
     };
 
+    /**
+     * Handler to select an imported OCPN.
+     * 
+     * @param event The event that triggered the click.
+     * @param index The index of the OCPN to select.
+     */
     const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
         setSelectedOCPN(index);
     }
@@ -141,7 +191,7 @@ const Home = () => {
                             }
                         </Box>
                     )}
-                    {(tabValue === 0 || !isSmallScreen) && <VisualizationArea selectedOCPN={selectedOCPN ? importedObjects[selectedOCPN] : null}/>}
+                    {(tabValue === 0 || !isSmallScreen) && <VisualizationArea selectedOCPN={selectedOCPN ? importedObjects[selectedOCPN] : null} />}
                 </Box>
             </Box>
             <ImportDialog
